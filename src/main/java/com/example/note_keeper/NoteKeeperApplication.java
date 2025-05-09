3f9a874aef42e2f73d2aa42153309d52fa662fb4
kadyy <ahmedkhaled12004@gmail.com>
2025-05-09 07:42:34 +0300

    initial commit with clean architecture

---
+++ src/main/java/com/example/note_keeper/NoteKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.note_keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteKeeperApplication.class, args);
+	}
+
+}
